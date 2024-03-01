8920b05ddcaf4996a09315319c22a9abfd13ecf2
yashpatilyp <yashpatilyp452001@gmail.com>
2024-03-01 10:12:07 +0530

    hosting url change

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = "http://localhost:5000";
 
-export const API_BASE_URL = "https://e-commerce-backend-tdca.onrender.com";
+export const API_BASE_URL = "https://e-commerce-backend-mla4.onrender.com";
