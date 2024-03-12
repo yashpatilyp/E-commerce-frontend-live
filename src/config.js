417de0ff4041ae030ecdff4f62d7953ef91ece31
yashpatilyp <yashpatilyp452001@gmail.com>
2024-03-12 13:15:04 +0530

    config update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "http://localhost:5000";
+// export const API_BASE_URL = "http://localhost:5000";
 
-// export const API_BASE_URL = "https://e-com-backend-7gy0.onrender.com";
+export const API_BASE_URL = "https://e-com-backend-7gy0.onrender.com";
  
 export const CLOUDINARY_CLOUD_NAME= "dfm5wkzjq"
